daf631a7accac4af0aff62908d9d8a1a16fdaa7d
MatthewSchuette <mjschuette4@gmail.com>
2020-06-02 16:05:34 -0500

    Main.java setup complete

---
+++ src/wordCount/Main.java
@@ -0,0 +1,13 @@
+package wordCount;
+
+import java.util.*;
+
+public class Main {
+    private static void countAllWords() {
+        
+    }
+
+    public static void main(String[] args) {
+        countAllWords();
+    }
+}
